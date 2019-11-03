92468d43fb8e33c85254ff45f0f7b6cca32a37a9
3daddict <print@3daddict.com>
2019-11-03 15:50:54 -0800

    return json example

--- http/server.js
+++ http/server.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
+const todos = [
+    {id: 1, text: 'Todo One'},
+    {id: 2, text: 'Todo Two'},
+    {id: 3, text: 'Todo Three'}
+];
+
 const server = http.createServer((req, res) => {
-    const { headers, url, method } = req;
-    console.log( headers, url, method );
-    res.end();
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('X-Powered-By', 'Node.js');
+    res.end(JSON.stringify({
+        success: true,
+        data: todos
+    }));
 });
 
 const PORT = 5000;
