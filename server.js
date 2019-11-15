b40c06cfd4d46946521d547f4edacf87d6176ac1
3daddict <print@3daddict.com>
2019-11-15 12:29:41 -0800

    router, controllers setup

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 //Load env
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-})
 
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-})
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-})
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-})
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
