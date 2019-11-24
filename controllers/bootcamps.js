ae74034f9252e6dd8c4ee3bb8f1356e3579c632f
3daddict <print@3daddict.com>
2019-11-24 12:19:20 -0800

    added pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     // Copy req.query
     const reqQuery = { ...req.query };
     // Fields to exlude
-    const removeFields = ['select', 'sort'];
+    const removeFields = ['select', 'sort', 'page', 'limit'];
     // Loop over remove fields and delete them from reqQuery
     removeFields.forEach(param => delete reqQuery[param]);
     // Create query string
@@ -32,11 +32,38 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     } else {
         query = query.sort('-createdAt');
     }
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 25;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Bootcamp.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
     // Executing query
     const bootcamps = await query;
+
+    //Pagination result
+    const pagination = {};
+
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
+
     res.status(200).json({
         success: true,
         count: bootcamps.length,
+        pagination,
         data: bootcamps
     }) 
 });
