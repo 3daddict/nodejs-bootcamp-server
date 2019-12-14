9dec23fbf9b5c87af95fac9fe8a72e396d9e93f9
3daddict <print@3daddict.com>
2019-12-14 11:43:38 -0800

    authorization middleware and protect created

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -15,25 +15,27 @@ const advancedResults = require('../middleware/advancedResults');
 // Incluse other resource routers
 const courseRouter = require('./courses');
 
-
 const router = express.Router();
+
+// Protect route middleware for auth
+const { protect } = require('../middleware/auth');
 
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router.route('/:id/photo').put(protect, bootcampPhotoUpload);
 
 router
     .route('/')
     .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-    .post(createBootcamp);
+    .post(protect, createBootcamp);
 
 router
     .route('/:id')
     .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+    .put(protect, updateBootcamp)
+    .delete(protect, deleteBootcamp);
 
 module.exports = router;
--- routes/courses.js
+++ routes/courses.js
@@ -12,6 +12,9 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+// Protect route middleware for auth
+const { protect } = require('../middleware/auth');
+
 router.route('/')
     .get(advancedResults(Course, {
         path: 'bootcamp',
@@ -19,11 +22,11 @@ router.route('/')
         }), 
         getCourses
     )
-    .post(addCourse)
+    .post(protect, addCourse)
 
 router.route('/:id')
     .get(getCourse)
-    .put(updateCourse)
-    .delete(deleteCourse)
+    .put(protect, updateCourse)
+    .delete(protect, deleteCourse)
 
 module.exports = router;
