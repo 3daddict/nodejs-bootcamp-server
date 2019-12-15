f3f21242909c6bdd7d798b1fd3f2d7e8645928e8
3daddict <print@3daddict.com>
2019-12-15 10:51:00 -0800

    bootcamp and user db relationship

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -31,12 +31,23 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
-        const bootcamp = await Bootcamp.create(req.body);
+    // Add user to req.body
+    req.body.user = req.user.id;
 
-        res.status(201).json({
-            success: true,
-            data: bootcamp
-        });
+    // Check for published bootcamps
+    const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+    // If user is not admin, they can only add one bootcamp
+    if (publishedBootcamp && req.user.role != 'admin') {
+        return next(new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`, 400));
+    }
+
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: bootcamp
+    });
 });
 
 // @desc    Update new bootcamp
