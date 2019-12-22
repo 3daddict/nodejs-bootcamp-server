f044e6901c0efdd76b004dc8c59539ae2289292e
3daddict <print@3daddict.com>
2019-12-21 16:48:46 -0800

    troubleshooting server error

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,24 +1,23 @@
 const express = require('express');
-const { 
-    getBootcamps, 
-    getBootcamp, 
-    createBootcamp, 
-    updateBootcamp, 
-    deleteBootcamp, 
-    getBootcampsInRadius, 
-    bootcampPhotoUpload 
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+  bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
 const Bootcamp = require('../models/Bootcamp');
 
-// Incluse other resource routers
+// Include other resource routers
 const courseRouter = require('./courses');
 const reviewRouter = require('./reviews');
 
 const router = express.Router();
 
 const advancedResults = require('../middleware/advancedResults');
-// Protect route middleware for auth
 const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
@@ -27,17 +26,19 @@ router.use('/:bootcampId/reviews', reviewRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route('/:id/photo').put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 router
-    .route('/')
-    .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-    .post(protect, authorize('publisher', 'admin'), createBootcamp);
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router
-    .route('/:id')
-    .get(getBootcamp)
-    .put(protect, authorize('publisher', 'admin'), updateBootcamp)
-    .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
+  .route('/:id')
+  .get(getBootcamp)
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
