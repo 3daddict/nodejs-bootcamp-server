c181b524cfa53bddccef5cc5214b3fc96724845e
3daddict <print@3daddict.com>
2019-12-21 16:55:30 -0800

    still trying to fix server error

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,166 +1,200 @@
 const path = require('path');
-const ErrorResponse = require('../utils/ErrorResponse');
+const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
-// @desc    Get all bootcamps
-// @route   GET /api/v1/bootcamps
-// @access  Public
+// @desc      Get all bootcamps
+// @route     GET /api/v1/bootcamps
+// @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-    res.status(200).json(res.advancedResults); 
+  res.status(200).json(res.advancedResults);
 });
 
-// @desc    Get single bootcamps
-// @route   GET /api/v1/bootcamps/:id
-// @access  Public
+// @desc      Get single bootcamp
+// @route     GET /api/v1/bootcamps/:id
+// @access    Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
-        const bootcamp = await Bootcamp.findById(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
-        if(!bootcamp) {
-            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        }
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
 
-        res.status(200).json({ 
-            success: true, 
-            data: bootcamp 
-        });  
+  res.status(200).json({ success: true, data: bootcamp });
 });
 
-// @desc    Create new bootcamp
-// @route   POST /api/v1/bootcamps
-// @access  Private
+// @desc      Create new bootcamp
+// @route     POST /api/v1/bootcamps
+// @access    Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
-    // Add user to req.body
-    req.body.user = req.user.id;
+  // Add user to req,body
+  req.body.user = req.user.id;
 
-    // Check for published bootcamps
-    const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
 
-    // If user is not admin, they can only add one bootcamp
-    if (publishedBootcamp && req.user.role != 'admin') {
-        return next(new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`, 400));
+  // If the user is not an admin, they can only add one bootcamp
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `The user with ID ${req.user.id} has already published a bootcamp`,
+        400
+      )
+    );
+  }
+
+  const bootcamp = await Bootcamp.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: bootcamp
+  });
+});
+
+// @desc      Update bootcamp
+// @route     PUT /api/v1/bootcamps/:id
+// @access    Private
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  let bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+
+  bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({ success: true, data: bootcamp });
+});
+
+// @desc      Delete bootcamp
+// @route     DELETE /api/v1/bootcamps/:id
+// @access    Private
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to delete this bootcamp`,
+        401
+      )
+    );
+  }
+
+  bootcamp.remove();
+
+  res.status(200).json({ success: true, data: {} });
+});
+
+// @desc      Get bootcamps within a radius
+// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access    Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide dist by radius of Earth
+  // Earth Radius = 3,963 mi / 6,378 km
+  const radius = distance / 3963;
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+  });
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps
+  });
+});
+
+// @desc      Upload photo for bootcamp
+// @route     PUT /api/v1/bootcamps/:id/photo
+// @access    Private
+exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.params.id} is not authorized to update this bootcamp`,
+        401
+      )
+    );
+  }
+
+  if (!req.files) {
+    return next(new ErrorResponse(`Please upload a file`, 400));
+  }
+
+  const file = req.files.file;
+
+  // Make sure the image is a photo
+  if (!file.mimetype.startsWith('image')) {
+    return next(new ErrorResponse(`Please upload an image file`, 400));
+  }
+
+  // Check filesize
+  if (file.size > process.env.MAX_FILE_UPLOAD) {
+    return next(
+      new ErrorResponse(
+        `Please upload an image less than ${process.env.MAX_FILE_UPLOAD}`,
+        400
+      )
+    );
+  }
+
+  // Create custom filename
+  file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
+
+  file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
+    if (err) {
+      console.error(err);
+      return next(new ErrorResponse(`Problem with file upload`, 500));
     }
 
-    const bootcamp = await Bootcamp.create(req.body);
-
-    res.status(201).json({
-        success: true,
-        data: bootcamp
-    });
-});
-
-// @desc    Update new bootcamp
-// @route   PUT /api/v1/bootcamps/:id
-// @access  Private
-exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-        let bootcamp = await Bootcamp.findById(req.params.id);
-    
-        if(!bootcamp) {
-            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        }
-
-        // Make sure user is bootcamp owner
-        if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
-            return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401));
-        }
-
-        bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-    
-        res.status(200).json({ success: true, data: bootcamp });
-});
-
-// @desc    Delete new bootcamp
-// @route   DELETE /api/v1/bootcamps/:id
-// @access  Private
-exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-        const bootcamp = await Bootcamp.findById(req.params.id);
-    
-        if(!bootcamp) {
-            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-        }
-
-        // Make sure user is bootcamp owner
-        if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
-            return next(new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`, 401));
-        }
-
-        bootcamp.remove();
-    
-        res.status(200).json({ success: true, data: {} });
-});
-
-// @desc    Get bootcamps within a radius
-// @route   GET /api/v1/bootcamps/radius/:zipcide/:distance
-// @access  Private
-exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
-    const { zipcode, distance } = req.params;
-
-    // Get lat and long from geocoder
-    const loc = await geocoder.geocode(zipcode);
-    const lat = loc[0].latitude;
-    const long = loc[0].longitude;
-
-    // Calculate the radius using radians
-    // Divide the distance by radius of the Earth
-    // Radius of Earth is 3,963 miles or 6,378.1 km
-    const radius = distance / 3963;
-
-    const bootcamps = await Bootcamp.find({
-        location: { $geoWithin: { $centerSphere: [ [ long, lat ], radius ] } }
-    });
+    await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name });
 
     res.status(200).json({
-        success: true,
-        count: bootcamps.length,
-        data: bootcamps
+      success: true,
+      data: file.name
     });
-});
-
-// @desc    Upload photo for bootcamp
-// @route   PUT /api/v1/bootcamps/:id/photo
-// @access  Private
-exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findById(req.params.id);
-
-    if(!bootcamp) {
-        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
-    }
-
-    // Make sure user is bootcamp owner
-    if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
-        return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401));
-    }
-
-    if(!req.files) {
-        return next(new ErrorResponse(`Please upload a file`, 400));
-    }
-
-    const file = req.files.file;
-
-    // Make sure the image is a photo
-    if(!file.mimetype.startsWith('image')) {
-        return next(new ErrorResponse(`Please upload an image file`, 400));
-    }
-
-    // Check filesize
-    if(file.size > process.env.MAX_FILE_UPLOAD) {
-        return next(new ErrorResponse(`Please upload an image less than ${MAX_FILE_UPLOAD} bytes`, 400));
-    }
-
-    // Create custom filename
-    file.name = `photo_${bootcamp._id}${path.parse(file.name).ext}`;
-
-    file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err => {
-        if(err) {
-            console.log(err);
-            return next(new ErrorResponse(`Problem with file upload`, 500));
-        }
-
-        await Bootcamp.findByIdAndUpdate(req.params.id, { photo: file.name })
-        res.status(200).json({ success: true, data: file.name });
-    });
+  });
 });
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { 
-    getReviews,
-    getReview,
-    addReview,
-    updateReview,
-    deleteReview
+const {
+  getReviews,
+  getReview,
+  addReview,
+  updateReview,
+  deleteReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -12,21 +12,23 @@ const Review = require('../models/Review');
 const router = express.Router({ mergeParams: true });
 
 const advancedResults = require('../middleware/advancedResults');
-// Protect route middleware for auth
 const { protect, authorize } = require('../middleware/auth');
 
-router.route('/')
-    .get(advancedResults(Review, {
-        path: 'bootcamp',
-        select: 'name description'
-        }), 
-        getReviews
-    )
-    .post(protect, authorize('user', 'admin'), addReview)
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description'
+    }),
+    getReviews
+  )
+  .post(protect, authorize('user', 'admin'), addReview);
 
-router.route('/:id')
-    .get(getReview)
-    .put(protect, authorize('user', 'admin'), updateReview)
-    .delete(protect, authorize('user', 'admin'), deleteReview)
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize('user', 'admin'), updateReview)
+  .delete(protect, authorize('user', 'admin'), deleteReview);
 
 module.exports = router;
